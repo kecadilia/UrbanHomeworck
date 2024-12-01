7fb6a22a327f9b4ab524b1f63ad28459ddaf3d5a
kecadilia <saha.krasilnikov.bol@gmail.com>
2024-12-01 16:45:06 +0300

    hello

---
+++ modul1/homeworck.py
@@ -0,0 +1,8 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students_list = sorted(students)
+averge_grades = {}
+for i in range(len(students_list)):
+    students_name = students_list[1]
+    students_grades = grades[1]
+    averge_grades = sum(students_grades) / len(students_grades)
