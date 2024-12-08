44fa074e9580448c3217f4a0faea4e573df937cb
kecadilia <saha.krasilnikov.bol@gmail.com>
2024-12-08 15:11:51 +0300

    oymay

--- modul1/homeworck.py
+++ modul1/homeworck.py
@@ -1,8 +1,19 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+# Преобразуем множество студентов в отсортированный список, чтобы соответствовать порядку оценок
 students_list = sorted(students)
-averge_grades = {}
+
+# Создаем словарь для хранения средних баллов
+average_grades = {}
+
+# Вычисляем средний балл для каждого студента
 for i in range(len(students_list)):
-    students_name = students_list[1]
-    students_grades = grades[1]
-    averge_grades = sum(students_grades) / len(students_grades)
+    student = students_list[i]
+    student_grades = grades[i]
+    average_grade = sum(student_grades) / len(student_grades)  # Вычисляем средний балл
+    average_grades[student] = average_grade  # Добавляем в словарь
+
+# Выводим результаты на экран
+for student, avg in average_grades.items():
+    print(f"{student}: {avg:.2f}")
